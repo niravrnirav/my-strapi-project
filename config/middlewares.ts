29f83725770a9c8d76d0c3c6ae0ffbd6aaa5127d
Nirav Patel <npatel@obsglobal.com>
2024-09-23 11:52:54 -0500

    added blog-post

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,26 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'your-domain.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // Allow requests from your React frontend
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
